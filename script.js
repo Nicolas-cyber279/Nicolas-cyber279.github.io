6e19e5fe2a9c06023b14112374ca28686826002f
Nicolas-cyber279 <valadaresn78@gmail.com>
2026-09-25 23:23:35 -0300

    Corrigido "Meus projetos", criado o rodapé, feito a função de limpar o formulário de contato no JS.

---
+++ script.js
@@ -0,0 +1,8 @@
+function LimparFormulario() {
+    document.getElementById("inpNome").value = "";
+    document.getElementById("inpTel").value = "";
+    document.getElementById("inpEmail").value = "";
+    document.getElementById("inpMensagem").value = "";
+
+    alert("Formulário limpo!");
+}
